d0c222b0114b4bc440c8abcb8232480e25f2a66f
easilo <easilo@ucsc.edu>
2019-05-10 15:49:05 -0700

    Added more platforms

--- phaser/Temperance Of The Mind/js/states/stage1.js
+++ phaser/Temperance Of The Mind/js/states/stage1.js
@@ -72,11 +72,11 @@ Stage1.prototype = {
 		platform1.body.immovable = true;
 		platform1.scale.setTo(2, 2);
 
-		var platform2 = platforms.create(-150, 200, 'platform01');
+		var platform2 = platforms.create(1000, 200, 'platform01');
 		platform2.body.immovable = true;
 		platform2.scale.setTo(2, 2);
 
-		var platform3 = platforms.create(-200, 600, 'platform01');
+		var platform3 = platforms.create(700, 300, 'platform01');
 		platform3.body.immovable = true;
 		platform3.scale.setTo(2, 2);
 
