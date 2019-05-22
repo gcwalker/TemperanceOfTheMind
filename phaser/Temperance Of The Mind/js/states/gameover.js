8712fb8258e264a736c0a4477b17f8c146ec440a
tormod45 <38771401+tormod45@users.noreply.github.com>
2019-05-22 16:52:51 -0700

    Update gameover.js

--- phaser/Temperance Of The Mind/js/states/gameover.js
+++ phaser/Temperance Of The Mind/js/states/gameover.js
@@ -2,8 +2,13 @@
 
 var GameOver = function(game) {};
 GameOver.prototype = {
+	preload: function() {
+		console.log('GameOver: preload');
+		
+	},
+	
 	create: function() {
-		var replayText = game.add.text(game.width/2, 400, ' Press SPACEBAR to Restart', {font: 'Helvetica', fontSize: '24px', fill: '#fff'});
+		var replayText = game.add.text(game.width/2, 400, 'Sorry you died buddy,  Press SPACEBAR to Restart', {font: 'Helvetica', fontSize: '24px', fill: '#fff'});
 		replayText.anchor.set(0.5);
 	},
 	update: function() {
