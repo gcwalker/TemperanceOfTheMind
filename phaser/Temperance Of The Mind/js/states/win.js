35c41f280da490441ab2f2066bc1e8f84272f342
easilo <easilo@ucsc.edu>
2019-06-07 16:53:54 -0700

    Finished win screen

--- phaser/Temperance Of The Mind/js/states/win.js
+++ phaser/Temperance Of The Mind/js/states/win.js
@@ -3,20 +3,23 @@
 var Win = function(game) {};
 Win.prototype = {
 	create: function() {
-		var congrats = game.add.text(game.width/2, 350, 'You defeated your demons!',{font: 'Helvetica', fontSize: '24px', fill: '#fff'});
-		congrats.anchor.set(0.5);
-		var replayText = game.add.text(game.width/2, 400, 'Press SPACEBAR to Restart', {font: 'Helvetica', fontSize: '24px', fill: '#fff'});
-		replayText.anchor.set(0.5);
 		var img = game.add.sprite(game.width/2,game.height/2,'winImg');
 		img.anchor.setTo(0.5);
-		img.scale.setTo(0.3);
+
+		var congrats = game.add.text(game.width/2, 450, 'You’ve quelled the despondent demons that have\nbeen plaguing your mind and you are now free\nof your destructive thoughts! ',{font: 'Press Start 2P', fontSize: '20px', fill: '#fff'});
+		congrats.anchor.set(0.5);
+		congrats.align = 'center';
+		var replayText = game.add.text(game.width/2, 650, 'Press SPACEBAR to Restart', {font: 'Press Start 2P', fontSize: '24px', fill: '#fff'});
+		replayText.anchor.set(0.5);
+
 		//music.stop();
 		music = game.add.audio('win');
 		music.play();
 	},
 	update: function() {
 		if(game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR)) {
-			game.state.start('Stage1');
+			music.stop();
+			game.state.start('Title');
 		}
 	}
 }
